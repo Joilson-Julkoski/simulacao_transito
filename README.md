dc2d01e1963a6bf9900653f21a7bf749d3efffe7
Joilson-Julkoski <joilson7548@gmail.com>
2023-06-16 23:38:21 -0300

    Add: - Function to create a signal - On create a car you chose if a car moves in X or Y - A cars per second counter - Spawn system

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 TO-DO list
 
-- [ ] Make a cars spawn system  
+- [ ] Make a moviment in Y vector | DOING
+- [x] Make a cars spawn system      
 - [ ] Make a street creation/edit system
 - [ ] Make the traffic light orange siganl system 
-- [ ] Make the counters
+- [x] Make the counters
 - [ ] Make the car sprite
 - [ ] Make the traffic light sprite
 - [ ] Make the road sprite
 - [ ] Make the graphics
 - [ ] Improve the readme
+- [ ] Timer to open and close the traffic light 
--- main.lua
+++ main.lua
@@ -1,4 +1,12 @@
-function createCar(x, y, aceleracao, maxVelx, freio, observer)
+function createSignal()
+    sinaleiro = {}
+    sinaleiro.name = "sinaleiro"
+    sinaleiro.open = false
+    sinaleiro.x = 500
+    sinaleiro.y = 300
+end
+
+function createCar(x, y, aceleracao, maxVelx, freio, observer, directionAxis, isInverted)
     local car = {}
     car.name = "car"
     car.x = x
@@ -8,12 +16,12 @@ function createCar(x, y, aceleracao, maxVelx, freio, observer)
     car.aceleracao = aceleracao
     car.freio = freio
     car.observer = observer
+    car.direction = directionAxis
     car.acelerar = function(dt)
         if car.vel < maxVelx then
             car.vel = car.vel + car.aceleracao * dt
         end
     end
-
 
     car.freiar = function(dt)
         if car.vel > 0 then
@@ -23,9 +31,12 @@ function createCar(x, y, aceleracao, maxVelx, freio, observer)
         end
     end
 
-
     car.main = function(dt)
-        car.x = car.x + car.vel * dt
+        if car.direction == "x" then
+            car.x = car.x + car.vel * dt
+        elseif car.direction == "y" then
+            car.y = car.y + car.vel * dt
+        end
         if car.observer == nil then
             car.acelerar(dt)
         else
@@ -43,47 +54,57 @@ function createCar(x, y, aceleracao, maxVelx, freio, observer)
 end
 
 function love.load()
-    sinaleiro = {}
-    sinaleiro.name = "sinaleiro"
-    sinaleiro.open = false
-    sinaleiro.x = 500
-    sinaleiro.y = 100
-    listOfCars = { createCar(10, 200, 100, 100, 200, sinaleiro) }
-
-    for i = 1, 10, 1 do
-        table.insert(listOfCars, createCar(10, 200, 100, 300, 500, listOfCars[i]))
-    end
+    createSignal()
+    listOfCars = { createCar(10, 200, 100, 100, 200, sinaleiro, "x") }
+    openTime = 0
+    count = 0 
 end
 
 function love.update(dt)
+    if listOfCars[#listOfCars].x > 20 then
+        table.insert(listOfCars, createCar(10, 200, 100, 300, 200, listOfCars[#listOfCars], "x"))
+    end
     if love.keyboard.isDown("space") then
         sinaleiro.open = true
     else
         sinaleiro.open = false
+        if count ~= 0 then
+            count = 0
+            openTime = 0
+        end
     end
-
+    if sinaleiro.open  == true then        
+        openTime = openTime + dt
+    end
     for index, value in ipairs(listOfCars) do
         value.main(dt)
 
         if value.x > sinaleiro.x and value.observer ~= nil then
+            count = count + 1
             listOfCars[index + 1].observer = sinaleiro
             value.observer = nil
         end
+        if value.x > love.graphics.getWidth() then
+            table.remove(listOfCars, index)
+        end
     end
+    carsPerSecond = count / openTime
 end
 
 function love.draw()
     for index, value in ipairs(listOfCars) do
-        love.graphics.rectangle("fill", value.x, 100, 10, 10)
+        love.graphics.rectangle("fill", value.x, value.y, 10, 10)
     end
 
     if sinaleiro.open then
         love.graphics.setColor(0, 255, 0)
-        love.graphics.rectangle("fill", sinaleiro.x, 100, 10, 10)
+        love.graphics.rectangle("fill", sinaleiro.x, sinaleiro.y, 10, 10)
     else
         love.graphics.setColor(255, 0, 0)
-        love.graphics.rectangle("fill", sinaleiro.x, 100, 10, 10)
+        love.graphics.rectangle("fill", sinaleiro.x, sinaleiro.y, 10, 10)
     end
 
     love.graphics.setColor(255, 255, 255)
+    
+    love.graphics.print(carsPerSecond .. " cars per second")
 end
